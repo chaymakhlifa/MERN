a7b4f0b5443aa8cd1023afc01b02aa58fb29fe43
Your Name <chaymakhlifa.contact@gmail.com>
2023-01-25 10:10:50 +0100

    First React

---
+++ Week 1/Day 1/Practise/First React With JSX/chayma/src/App.js
@@ -0,0 +1,18 @@
+ import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+           <h1>Hello Dojo!</h1>
+            <h3> Things I need to do :</h3> 
+            <h5>*Larn React</h5>  
+            <h5>*Climb MT. Everest</h5> 
+            <h5>*Run a marathon </h5> 
+            <h5>*Feed the dogs</h5>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+    </div>
+  );
+}
+
+export default App;
--- Week 1/Day 1/Practise/First React/chayma/src/App.js
+++ Week 1/Day 1/Practise/First React/chayma/src/App.js
@@ -1,15 +1,20 @@
- import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-           <h1>Hello Dojo!</h1>
-            <h3> Things I need to do :</h3> 
-            <h5>*Larn React</h5>  
-            <h5>*Climb MT. Everest</h5> 
-            <h5>*Run a marathon </h5> 
-            <h5>*Feed the dogs</h5>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+      <h1>First React page rendering...</h1>
+
+    <script crossorigin="" src="https://unpkg.com/react@16/umd/react.development.js"></script>
+    <script crossorigin="" src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script> 
+
+
+    <script>
+       const App = React.createElement("h1", {}, "Our First React page has rendered");
+       ReactDOM.render(App, document.getElementById("root"));
+    </script>
+
 
     </div>
   );
