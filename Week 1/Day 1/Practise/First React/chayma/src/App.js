4ce2f462304dd8b16c3c52bc6fbde424f5e7be85
Your Name <chaymakhlifa.contact@gmail.com>
2023-01-25 09:56:12 +0100

    First React With JSX

--- Week 1/Day 1/Practise/First React/chayma/src/App.js
+++ Week 1/Day 1/Practise/First React/chayma/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-            <h1>First React page rendering...</h1>
-    <script>
-    const App = React.createElement("h1", {}, "Our First React page has rendered");
-    ReactDOM.render(App, document.getElementById("root"));
-    </script>
-
+           <h1>Hello Dojo!</h1>
+            <h3> Things I need to do :</h3> 
+            <h5>*Larn React</h5>  
+            <h5>*Climb MT. Everest</h5> 
+            <h5>*Run a marathon </h5> 
+            <h5>*Feed the dogs</h5>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
     </div>
   );
