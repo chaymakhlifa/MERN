e43ff3e467942f24f76bb67c33fcca195dd41190
Your Name <chaymakhlifa.contact@gmail.com>
2023-02-03 13:36:51 +0100

    Box Generator

---
+++ Week 2/Box Generator/box/src/App.js
@@ -0,0 +1,13 @@
+import Box from "./components/Box"
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week 2/Box Generator/box/src/components/Box.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Box = ()=>{
+    return (
+        <div>
+            <h1>Color</h1>
+            <input className="sun"></input>
+            <button className="btn">Add</button>
+            <div className="box">
+                <div className="card">
+                </div>
+                <div className="card-1">
+                </div>
+                <div className="card-2">
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+
+
+export default Box;
