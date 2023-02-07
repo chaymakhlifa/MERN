37d82c3afc152223209b85add98c7d1369203a50
Your Name <chaymakhlifa.contact@gmail.com>
2023-02-07 11:24:30 +0100

    Pokemon API

--- week 3/Day 2/Pokemon API/pokemon/src/App.js
+++ week 3/Day 2/Pokemon API/pokemon/src/App.js
@@ -1,36 +1,14 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Pokemon from './Components/Pokemon';
 
 function App() {
-  
-  const [pokemon, setPokemon] = useState([]);
-
-  useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-      .then(response => {
-    
-        return response.json()
-      })
-      .then(response => {
-        setPokemon(response.results)
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-    
-  }, []);  
-
   return (
-    <div className="App" style={{ width: "200px", margin: "auto" }}>
-      <ul>
-        {
-          pokemon.map((pokemonObj, index)=>{
-            return (<li key={index}>{pokemonObj.name}</li>)
-          })
-        }
-      </ul>
+    <div className='App'>
+    <Pokemon/>
+
     </div>
-  );
+  )
 }
 
 export default App;
